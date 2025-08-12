384403f52871c8a9cb68876317112aa9796e32bc
UYWASAS <uywasas@gmail.com>
2025-08-12 11:42:04 -0500

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,25 +1,12 @@
-def convert_unit(valor: float, from_unit: str, to_unit: str) -> float:
-    # TODO: Implementar conversión unidad energía (kcal/MJ)
+def convert_unit(valor, from_unit, to_unit):
     return valor
 
-def check_range(valor: float, vmin: float, vmax: float) -> bool:
+def check_range(valor, vmin, vmax):
     return vmin <= valor <= vmax
 
-def convert_asfed_ms(valor: float, ms: float, from_base: str, to_base: str) -> float:
-    """Convierte entre base MS y as-fed dado el %MS."""
-    if from_base == to_base:
-        return valor
-    if from_base == "MS" and to_base == "as-fed":
-        return valor * (ms/1000)
-    elif from_base == "as-fed" and to_base == "MS":
-        return valor * (1000/ms)
-    else:
-        return valor
+def convert_asfed_ms(valor, ms, from_base, to_base):
+    return valor
 
-def show_cheatsheet(familia:str):
-    # TODO: Mostrar variables mínimas sugeridas por familia
-    if familia.lower().startswith("cereal"):
-        st.info("Variables mínimas recomendadas: CP, EE, CF, Ash, Starch/NFE")
-    elif familia.lower().startswith("oleaginosas"):
-        st.info("Variables mínimas: CP, EE, CF, Ash, antinutrientes si aplica")
-    # ...
+def show_cheatsheet(familia):
+    import streamlit as st
+    st.info(f"Cheat-sheet para {familia} (stub)")
